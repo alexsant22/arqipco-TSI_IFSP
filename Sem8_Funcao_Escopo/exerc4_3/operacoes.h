66e9e36c6f09adb1355758289af192b09e86dd2a
Alexandre Santos <alexandre.santana2201@gmail.com>
2026-04-29 22:08:29 -0300

    feat: implementa calculadora com operações básicas e tratamento de divisão por zero

---
+++ Sem8_Funcao_Escopo/exerc4_3/operacoes.h
@@ -0,0 +1,12 @@
+// operacoes.h
+
+#ifndef OPERACOES_H
+#define OPERACOES_H
+
+// Protótipos das funções
+float adicao(float a, float b);
+float subtracao(float a, float b);
+float multiplicacao(float a, float b);
+float divisao(float a, float b);
+
+#endif
